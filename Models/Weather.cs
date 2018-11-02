ad28c7d16f086b17ac8ec5e4883452425787cd70
광수 박 <kspark@taihoinst.onmicrosoft.com>
2018-11-02 18:40:22 +0900

    20181102- weather

---
+++ Models/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaihoChatBotV3.Models
+{
+    public partial class Weather
+    {
+
+        
+        
+
+    }
+}
---
+++ Models/WeatherList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaihoChatBotV3.Models
+{
+    public class WeatherList
+    {
+        public string weather;
+        public string temperature_string;
+        public string temp_c;
+        public string relative_humidity;
+        public string wind_string;
+        public string wind_dir;
+        public string icon_url;
+
+    }
+}
